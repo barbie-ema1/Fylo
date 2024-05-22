1e4d2ba968c4d434de21fe6054b2721b1b528064
barbieema-1 <mhizemmyblaze@gmail.com>
2024-05-22 14:48:55 -0700

    modified:   index.html 	new file:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,14 @@
+function Fun() {
+    document.getElementById("mySubmit").formTarget = "_self";
+    document.getElementById("in").style.borderColor = "red";
+    document.getElementById("demo").innerHTML = "Please check your email";
+
+}
+
+
+function myFunction() {
+    
+    var x = document.getElementById("mySubmitt").formTarget;
+    document.getElementById("demoo").innerHTML = x;
+    document.getElementById("inP").style.borderColor = "red";
+  }
